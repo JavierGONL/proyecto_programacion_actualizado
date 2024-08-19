dbf8b7177186aa0b914b11dd7b873df812b1e344
David-A-Montes-R <158982203+David-A-Montes-R@users.noreply.github.com>
2024-08-18 22:06:40 -0500

    Update recorrido.py

--- recorrido.py
+++ recorrido.py
@@ -1,4 +1,5 @@
 from simulacion_digital import *
 puerta=["and","or"]
-recopilatorio_simulaciones(puerta[0])
-recopilatorio_simulaciones(puerta[1])
+while True:
+    recopilatorio_simulaciones(puerta[0])
+    recopilatorio_simulaciones(puerta[1])
